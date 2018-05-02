46cfdbb69e69e254c2793cd89de80ff5babf32c6
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-05-02 21:34:50 +0200

    Add colors:
    Based on this: https://github.com/Tarrasch/zsh-colors

--- home/.config/sh/modules-available/options.zsh
+++ home/.config/sh/modules-available/options.zsh
@@ -18,9 +18,9 @@ zstyle :compinstall filename '/Users/atuin/.zshrc'
 zstyle ':completion:*' menu select
 zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)*==34=34}:${(s.:.)LS_COLORS}")'
 
-autoload -Uz promptinit; promptinit
+autoload -Uz promptinit && promptinit
 
-autoload -Uz bashcompinit; bashcompinit
+autoload -Uz bashcompinit && bashcompinit
 
 # Custom autoload functions
 autoload cdg \
@@ -28,6 +28,11 @@ autoload cdg \
     lsp \
     tardir \
     bandcamp-dl
+
+# Colors by name - based on https://github.com/Tarrasch/zsh-colors
+colors=(black red green yellow blue magenta cyan white)
+autoload -Uz $colors
+autoload -U colors && colors
 
 # Keybinding
 bindkey -v
