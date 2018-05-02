0078824e3958b76c6e5f3dec21f523824976c346
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-05-02 21:58:09 +0200

    Add completion to homeshick

--- home/.config/sh/modules-available/homeshick.zsh
+++ home/.config/sh/modules-available/homeshick.zsh
@@ -1,7 +1,7 @@
 # Homeshick
 
 [[ -f $HOME/.homesick/repos/homeshick/homeshick.sh ]] && {
-    source $HOME/.homesick/repos/homeshick/homeshick.sh
     fpath+=~/.homesick/repos/homeshick/completions
+    source $HOME/.homesick/repos/homeshick/homeshick.sh
 }
 
