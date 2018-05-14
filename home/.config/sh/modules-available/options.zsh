3038ff4df629b482097ae8e267e6164c3068b8b3
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-05-14 23:31:48 +0200

    Fix theme loading

--- home/.config/sh/modules-available/options.zsh
+++ home/.config/sh/modules-available/options.zsh
@@ -11,9 +11,9 @@ unsetopt beep
 
 fpath+=~/.zsh/completions
 fpath+=~/.zsh/autoload
+fpath+=~/.zprompts
 
 # autoload some stuff
-zstyle :compinstall filename '/Users/atuin/.zshrc'
 zstyle ':completion:*' menu select
 zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)*==34=34}:${(s.:.)LS_COLORS}")'
 
