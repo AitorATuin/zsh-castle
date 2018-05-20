6faeb5e7574f6ca58182ec3fe2f735f525c05963
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-05-20 10:44:54 +0200

    Add alias to start firefox in firejail and a watch ss command

--- home/.config/sh/modules-available/aliases.zsh
+++ home/.config/sh/modules-available/aliases.zsh
@@ -7,3 +7,8 @@ alias mpcc=ncmpcpp -h ~/.mpd/socket
 alias m5s="md5sum $@"
 
 alias ls="ls --color=always"
+
+# firejail
+FIREJAIL_DIR=~/.firejail
+FIREFAIL_PROFILES=~/.firejail/profiles
+alias firefox-dev="firejail --name=firefox-dev --profile=${FIREFAIL_PROFILES}/firefox.profile --private=${FIREJAIL_DIR}/firefox-dev /opt/firefox/firefox --no-remote $@"
