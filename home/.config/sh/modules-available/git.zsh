ad3501518ae2b0e180e7e9fc4665249a01cb08de
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-05-20 23:13:13 +0200

    Add simple git aliases

---
+++ home/.config/sh/modules-available/git.zsh
@@ -0,0 +1,14 @@
+# :vi ft=zsh
+
+# Some aliases
+alias ga='git add'
+alias gs='git status'
+alias gd='git diff'
+alias go='git checkout'
+alias gm='git commit -m'
+alias gb='git branch'
+alias gra='git remote add'
+alias grr='git remote rm'
+alias gpu='git pull'
+alias gcl='git clone'
+
