049e1969db196612e04630692488c22e19d6888b
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-05-20 23:18:31 +0200

    Add aliases to use highlight on cat and less

--- home/.config/sh/modules-available/aliases.zsh
+++ home/.config/sh/modules-available/aliases.zsh
@@ -11,3 +11,6 @@ alias ls="ls --color=always"
 alias firefox-dev="firefox firefox.profile firefox-dev firefox-dev"
 alias firefox-mail="firefox firefox.profile firefox-email firefox-mail"
 alias firefox-priv="firefox firefox.profile none firefox-priv"
+
+alias hless='f(){highlight -O truecolor $1 --force "${@:2}" | less}; f $@'
+alias hcat='f(){highlight -O truecolor $1 --force "${@:2}"}; f $@
