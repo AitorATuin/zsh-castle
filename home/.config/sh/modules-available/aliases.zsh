384db0287b98ddf94e9906ab8b188a56753bdc9e
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-05-20 23:13:57 +0200

    Fix firefox aliases

--- home/.config/sh/modules-available/aliases.zsh
+++ home/.config/sh/modules-available/aliases.zsh
@@ -8,6 +8,6 @@ alias m5s="md5sum $@"
 
 alias ls="ls --color=always"
 
-alias firefox-dev="firefox firefox firefox.profile firefox-dev firefox-dev"
-alias firefox-mail="firefox firefox firefox.profile firefox-email firefox-mail"
-alias firefox-priv="firefox firefox firefox.profile none firefox-priv"
+alias firefox-dev="firefox firefox.profile firefox-dev firefox-dev"
+alias firefox-mail="firefox firefox.profile firefox-email firefox-mail"
+alias firefox-priv="firefox firefox.profile none firefox-priv"
