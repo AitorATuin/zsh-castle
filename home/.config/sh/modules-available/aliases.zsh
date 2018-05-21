7454ce2088504ae5a137a8d6a19253f19478f139
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-05-21 22:03:03 +0200

    Fix typo in alias

--- home/.config/sh/modules-available/aliases.zsh
+++ home/.config/sh/modules-available/aliases.zsh
@@ -13,4 +13,4 @@ alias firefox-mail="firefox firefox.profile firefox-email firefox-mail"
 alias firefox-priv="firefox firefox.profile none firefox-priv"
 
 alias hless='f(){highlight -O truecolor $1 --force "${@:2}" | less}; f $@'
-alias hcat='f(){highlight -O truecolor $1 --force "${@:2}"}; f $@
+alias hcat='f(){highlight -O truecolor $1 --force "${@:2}"}; f $@'
