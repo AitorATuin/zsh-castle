f0cc75507ca6d772a99c79fe4d7fe487b2a36e2a
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-10-03 21:25:08 +0200

    Source nix files.
    I should move this to another file

--- home/.config/sh/modules-available/envs.zsh
+++ home/.config/sh/modules-available/envs.zsh
@@ -35,3 +35,7 @@ export FZF_DEFAULT_OPTS='
 '
 
 export POWERLINE_CONFIG_COMMAND=~/.local/opt/powerline/scripts/powerline-config
+
+# nix, move to its own file
+export NIX_REMOTE=daemon
+source /etc/profile.d/nix.sh
