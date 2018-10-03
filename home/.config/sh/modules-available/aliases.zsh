2eef296f87ef7b8e9340106a84b1239b52feb0d4
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-10-03 21:24:55 +0200

    Add 2 more firefox aliases

--- home/.config/sh/modules-available/aliases.zsh
+++ home/.config/sh/modules-available/aliases.zsh
@@ -10,7 +10,9 @@ alias ls="ls --color=always"
 
 alias firefox-dev="firefox firefox-dev --private=firefox-dev"
 alias firefox-mail="firefox firefox-email --private=firefox-email"
+alias firefox-sas="firefox firefox-sas --private=firefox-sas"
 alias firefox-priv="firefox firefox-priv"
+alias firefox-htb="firefox firefox-htb --private=firefox-htb"
 
 alias hless='f(){highlight -O truecolor $1 --force "${@:2}" | less}; f $@'
 alias hcat='f(){highlight -O truecolor $1 --force "${@:2}"}; f $@'
