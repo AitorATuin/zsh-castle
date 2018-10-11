c5eaa6494b9473e279711060aa07d2ff5889eaef
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-10-11 22:48:39 +0200

    Add zsh-nix-shell
    This plugin enables zsh in nix-shell environments

--- home/.config/sh/modules-available/nix.zsh
+++ home/.config/sh/modules-available/nix.zsh
@@ -2,6 +2,8 @@
 
 export NIX_REMOTE=daemon
 source /etc/profile.d/nix.sh
+source $HOME/.local/opt/zsh-nix-shell/nix-shell.plugin.zsh
 source $HOME/.local/opt/nix-zsh-completions/nix.plugin.zsh
 fpath=($HOME/.local/opt/nix-zsh-completions $fpath)
 autoload -U compinit && compinit
+prompt_nix_shell_setup "$@"
