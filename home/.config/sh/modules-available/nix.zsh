73c645ff0009d50099e6ea037cc7d11293a2017c
AitorATuin <1757663+AitorATuin@users.noreply.github.com>
2018-11-18 22:08:46 +0100

    Add locale path for nix-shell

--- home/.config/sh/modules-available/nix.zsh
+++ home/.config/sh/modules-available/nix.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 export NIX_REMOTE=daemon
+export LOCALE_ARCHIVE=$HOME/.nix-profile/lib/locale/locale-archive
 source /etc/profile.d/nix.sh
 source $HOME/.local/opt/zsh-nix-shell/nix-shell.plugin.zsh
 source $HOME/.local/opt/nix-zsh-completions/nix.plugin.zsh
